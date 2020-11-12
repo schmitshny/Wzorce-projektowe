0235af107c9ae7b7f19e3885bc5fd55c96f0d284
Marcin Bugaj <mbugaj93@gmail.com>
2020-11-12 22:13:46 +0100

    Add project files.

---
+++ abs/abs/abs/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace labs 
+{
+
+    class Program
+    { static void Main(string[] args)
+        {
+            new Lab6AbstractFactory().Run();
+        }
+
+    }
+
+    class Lab6AbstractFactory
+    {
+        public void Run()
+        {
+            Console.WriteLine("Witamy w salonie z meblami");
+            ShowRoomProject(new IkeaFactory());
+        }
+        void ShowRoomProject(IRoomFactory roomFactory)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Budujemy pokoj w stylu " + roomFactory.GetType().Name);
+            Console.WriteLine("Pokoj będzię się składał z:");
+            IChair chair = roomFactory.createChair();
+            Console.WriteLine(chair.show());
+            ISofa sofa = roomFactory.createSofa();
+            Console.WriteLine(sofa.show());
+            Console.WriteLine();
+        }
+    }
+
+    interface IRoomFactory
+    {
+        public IChair createChair();
+        public ISofa createSofa();
+    }
+
+    class IkeaFactory : IRoomFactory
+    {
+        public IChair createChair()
+        {
+            return new MarkusChair();
+        }
+        public ISofa createSofa()
+        {
+            return new HemnesSofa();
+        }
+    }
+
+    interface IChair
+    {
+        public String show();
+    }
+    interface ISofa
+    {
+        public String show();
+    }
+
+    class MarkusChair : IChair
+    {
+        public String show()
+        {
+            return ("Markus chair");
+        }
+    }
+
+    class HemnesSofa : ISofa
+    {
+        public String show()
+        {
+            return "Hemnes sofa";
+        }
+    }
+}
---
+++ factory/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Numerics;
+
+namespace factory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Lab5FactoryMethod().Run();
+        }
+    }
+
+    class Lab5FactoryMethod
+    {
+        public void Run()
+        {
+            Console.WriteLine("Witamy na targach samochodowych");
+            MakeCarShow(new BmwFactory());
+            MakeCarShow(new SkodaFactory());
+        }
+        void MakeCarShow(CarFactory carFactory)
+        {
+            carFactory.PresentBrandNewCar();
+        }
+    }
+
+    abstract class CarFactory
+    {
+       public void PresentBrandNewCar()
+        {
+            ICar car = FactoryMethodMakeCar();
+            Console.WriteLine("Witamy na pokazie samochodów " + car.GetType().Name);
+            Console.WriteLine("Teraz zobaczmy jak działają kierunkowskazy");
+            car.ShowTurnSignal();
+            Console.WriteLine("Koniec pokazu.");
+            Console.WriteLine();
+        }
+        public abstract ICar FactoryMethodMakeCar();
+    }
+
+    class BmwFactory : CarFactory
+    {
+        public override ICar FactoryMethodMakeCar()
+        {
+            return new BMW();
+        }
+ 
+    }
+
+    class SkodaFactory : CarFactory
+    {
+        public override ICar FactoryMethodMakeCar()
+        {
+            return new Skoda();
+        }
+
+    }
+
+    interface ICar
+    {
+        public void ShowTurnSignal();
+    }
+    class BMW : ICar
+    {
+        public void ShowTurnSignal()
+        {
+            Console.WriteLine("Kierowcy i tak ich nie uzywają, więc w tym modelu nie dodaliśmy kierunkowskazów");
+        }
+    }
+
+    class Skoda : ICar
+    {
+        public void ShowTurnSignal()
+        {
+            Console.WriteLine("Zobaczcie pięknie migające migacze");
+        }
+    }
+}
+
